c3a52e71da2a7eb2647108fb92a5dde0ff374b64
Danh20042 <danhvtse182522@fpt.edu.vn>
2024-11-06 08:46:28 +0700

    Update UI detail Window

--- FlowerShop/obj/Debug/net8.0-windows/App.g.i.cs
+++ FlowerShop/obj/Debug/net8.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace FlowerShop {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/FlowerShop;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FlowerShop;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- FlowerShop/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
+++ FlowerShop/obj/Debug/net8.0-windows/LoginWindow.g.i.cs
@@ -88,7 +88,7 @@ namespace FlowerShop {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/FlowerShop;V1.0.0.0;component/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FlowerShop;component/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
