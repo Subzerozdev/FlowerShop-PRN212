adbbf00e6fbf9f77e2b4c73726c6eb280c2e7794
tokudosan12 <16.12t3.doquochuy@gmail.com>
2024-11-10 16:40:30 +0700

    chỉnh lại nút của user staff

--- FlowerShop/obj/Debug/net8.0-windows/App.g.i.cs
+++ FlowerShop/obj/Debug/net8.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace FlowerShop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace FlowerShop {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/FlowerShop;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/FlowerShop;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace FlowerShop {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public static void Main() {
             FlowerShop.App app = new FlowerShop.App();
             app.InitializeComponent();
